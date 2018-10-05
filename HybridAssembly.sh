a721df2d80261e6d86a2e41680b5404ec9950dac
Benjibendito <benjamin.j.perry@gmail.com>
2018-10-05 16:57:49 +1300

    v1.0.0 Working End-to-End

--- HybridAssembly.sh
+++ HybridAssembly.sh
@@ -33,8 +33,8 @@ sleep 1
 ###  Check for read files ###
 if [ -f $1 ]; then
 	ONTFILT=$1
-	printf "ONT Read File: $ONPREADS\n\n"
-    STRAIN=$(echo $ONTFILT | cut -d "." -f1)
+	printf "ONT Read File: $ONTFILT\n\n"
+    STRAIN=$(echo $ONTFILT | cut -d "." -f 1)
 else
 	printf "ERRORL: No file designated for ONT reads.\n"
 	printf "Usage: ./HybridAssembly.sh '$STRAIN'.chop.filt.*.fastq Illumina.*.R1.fastq.gz Illumina.*.R2.fastq.gz\n\n"
@@ -44,7 +44,7 @@ fi
 if [ -f $2 ] && [ -f $3 ]; then
 	ILLUMINAR1=$2
 	ILLUMINAR2=$3
-	printf "Illumina Read Files: $READ1 $READ2\n\n"
+	printf "Illumina Read Files: $ILLUMINAR1 $ILLUMINAR2\n\n"
 else
 	printf "ERROR: Illumina read files are not properly indicated.\n"
 	printf "Usage: ./HybridAssembly.sh '$STRAIN'.chop.filt.*.fastq Illumina.*.R1.fastq.gz Illumina.*.R2.fastq.gz\n\n"
@@ -62,17 +62,18 @@ source activate py36
 
 # Module 1
 # SPAdes K-mer Correction of Illumina Data py36
-spades.py -1 "$ILLUMINAR1" -2 "$ILLUMINAR2" -o "$SPADESCOROUT" --only-error-correction -t 14 -m 24
-SPADESCOROUTR1="$SPADESCOROUT"corrected/"$STRAIN"*R1*cor.fastq.gz
-SPADESCOROUTR2="$SPADESCOROUT"corrected/"$STRAIN"*R2*cor.fastq.gz
+# spades.py -1 "$ILLUMINAR1" -2 "$ILLUMINAR2" -o "$SPADESCOROUT" --only-error-correction -t 14 -m 24
+# SPADESCOROUTR1="$SPADESCOROUT"corrected/`ls "$SPADESCOROUT"corrected | grep -e "R1"`
+# SPADESCOROUTR2="$SPADESCOROUT"corrected/`ls "$SPADESCOROUT"corrected | grep -e "R2"`
 
 # Module 2
 # LoRDEC K-mer Correction of the Nanopore Long Reads py36
 mkdir "$LORDECCOROUT"
+LORDECCOROUTFILE="$LORDECCOROUT""$STRAIN".lordec.fasta
 ILLUMINASPADESCOR="$SPADESCOROUT"corrected/"$STRAIN".cat.cor.fastq.gz
-cat "$SPADESCOROUTR1" "$SPADESCOROUTR2" > "$ILLUMINASPADESCOR"
-lordec-correct -i "$ONTFILT" -2 "$ILLUMINASPADESCOR" -k 19 -s 3 -T 14 -p -o "$LORDECCOROUT"
-LORDECCOROUT="$LORDECCOROUT""$STRAIN".lordec.fasta
+# cat "$SPADESCOROUTR1" "$SPADESCOROUTR2" > "$ILLUMINASPADESCOR"
+lordec-correct -i "$ONTFILT" -2 "$ILLUMINASPADESCOR" -k 19 -s 3 -T 14 -p -o "$LORDECCOROUTFILE"
+
 
 # Exit py36 Environment
 source deactivate
@@ -81,7 +82,7 @@ source activate py27
 
 # Module 3
 # Flye Genome Assembly of the LoRDEC Corrected Long Reads py27
-flye --nano-corr "$LORDECCOROUT" -g 7m --out-dir "$FLYEOUT" -t 14
+flye --nano-corr "$LORDECCOROUTFILE" -g 7m --out-dir "$FLYEOUT" -t 14
 
 # Exit py27 Environment
 source deactivate
@@ -93,7 +94,7 @@ source activate py36
 FLYEGFA="$FLYEOUT"2-repeat/graph_final.gfa
 mkdir "$UNICYCLEROUT"
 
-unicycler -1 "$SPADESCOROUTR1" -2 "$SPADESCOROUTR2" -l "$LORDECCOROUT" --verbosity 4 --vcf -t 14 --existing_long_read_assembly "$FLYEGFA" -o "$UNICYCLEROUT"
+unicycler -1 "$SPADESCOROUTR1" -2 "$SPADESCOROUTR2" -l "$LORDECCOROUTFILE" --verbosity 3 --vcf -t 14 --existing_long_read_assembly "$FLYEGFA" -o "$UNICYCLEROUT"
 
 # Exit py36 Environment
 source deactivate
